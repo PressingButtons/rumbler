8f353f23fea4603f9ecd64fa64cf0a43b48400b0
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2023-04-10 10:12:51 -0500

    preserving changes to main

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,48 @@
-import initGraphics from "./graphics/graphics.js"
+import RenderWorkerES6 from "./render/render_worker-module.js";
+import GameWorkerES6 from "./game/gameworker_module.js";
 
 window.onload = async function(event) {
-    const base_uri = new URL('../', import.meta.url).href;
-    await initGraphics(base_uri);
-    console.log('graphics initialized');
+    const uri = new URL('./', import.meta.url);
+    const game_worker = new GameWorkerES6( );
+    const render_worker = new RenderWorkerES6(uri.href);
+    const result = await render_worker.init(document.getElementById('game'), game_worker).then(x => render_worker.preloadTextures());
+
+    let pos = [0, 0];
+
+    
+    function test(x, y) {
+        render_worker.render({
+            projection: {left: 0, right: 800, top: 0, bottom: 450},
+            objects: [{
+                position: [x, y, 0],
+                rotation: [0, 0, 0],
+                width: 50, height: 50,
+                render_detail: {
+                    buffer: 'square',
+                    program: 'single_texture',
+                    textures: [{
+                        name: 'placeholder',
+                        wrap_s: 'CLAMP_TO_EDGE',
+                        wrap_t: 'CLAMP_TO_EDGE',
+                    }],
+                    draw_method: 'TRIANGLE_STRIP',
+                    first_array: 0,
+                    indices: 4,
+                    tint: [1, 1, 1, 1],
+                }
+            }] 
+        })
+    }
+
+    document.addEventListener('keydown', event => {
+        if(event.key == 'd') pos[0] ++;
+        if(event.key == 'a') pos[0] --;
+        if(event.key == 'w') pos[1] --;
+        if(event.key == 's') pos[1] ++;
+        render_worker.fill(new Array(4).fill(0).map(x => Math.random( )));
+        test(...pos);
+    });
+
+
+
 }
