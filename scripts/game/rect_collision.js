bf73460cab733ceae44654c667f958eab1bfb433
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2023-05-06 10:25:56 -0500

    update to latest version of collider

--- scripts/game/rect_collision.js
+++ scripts/game/rect_collision.js
@@ -1,38 +1,26 @@
 const RectangluarCollider = function(  ) {
 
     function pointVsRect(point, rect) {
-        return (point.x >= rect.left && point.y >= rect.y && point.x < rect.right && point.y < rect.bottom);
+        return (point.x >= rect.left && point.y >= rect.top && point.x < rect.right && point.y < rect.bottom);
     }
 
     function rectVsRect(r1, r2) {
         return (r1.left < r2.right && r1.right > r2.left && r1.top < r2.bottom && r1.bottom > r2.top )
     }
 
-    function tNear(ray_origin, ray_dir, rect) {
-        const dividend = [rect.left - ray_origin[0], rect.top - ray_origin[1]];
-        return [dividend[0] / ray_dir[0], dividend[1] / ray_dir[1]];
-    }
-
-    function tFar(ray_origin, ray_dir, rect) {
-        const dividend = [rect.right / ray_origin[0], rect.bottom - ray_origin[1]];
-        return [dividend[0] / ray_dir[0], dividend[1] / ray_dir[1]];
-    }
-
-    function getRayIntersections(ray_origin, ray_dir, rect) {
-        let t_near = tNear(ray_origin, ray_dir, rect);
-        let t_far = tNear(ray_origin, ray_dir, rect);
-        if(t_near[0] > t_near[0]) [t_near[0], t_far[0]] = [t_far[0], t_near[0]];
-        if(t_near[1] > t_near[1]) [t_near[1], t_far[1]] = [t_far[1], t_near[1]];
+    function tHit(pos, ray_origin, ray_vector) {
         return {
-            near: t_near,
-            far: t_far
+            x: (pos.x - ray_origin.x) / ray_vector.x,
+            y: (pos.y - ray_origin.y) / ray_vector.y
         }
     }
 
+
     function getCollisionPoint(ray_origin, hit_near, ray_dir) {
-        const p = [hit_near[0] * ray_dir[0], hit_near[1] * ray_dir[1]];
-        p[0] += ray_origin[0]; p[1] += ray_origin[1];
-        return p;
+        const point = {x: (hit_near * ray_dir.x), y: (hit_near * ray_dir.y)}
+        point.x += ray_origin.x;
+        point.y += ray_origin.y;
+        return point;
     }
 
     function createNormal(t, ray_dir) {
@@ -41,27 +29,46 @@ const RectangluarCollider = function(  ) {
                 return [1, 0];
             else 
                 return [-1, 0];
-        else if (t_near[0] < t_near[1])
+        else if (t.near[0] < t.near[1])
             if(ray_dir[1] < 0) 
                 return [0, 1];
             else 
                 return [0, -1]; 
     }
 
-    function RayVsRect( ray_origin, ray_dir, rect, contact_point, contact_normal, hit_near) {
-        const it = getRayIntersections(ray_origin, ray_dir, rect);
+    function RayVsRect( ray_origin, ray_dir, rect, contact_point, contact_normal, t_hit_near) {
+
+        const t_near = { 
+            x: (rect.left - ray_origin.x) / ray_dir.x,
+            y: (rect.top  - ray_origin.y) / ray_dir.y,
+        }
+
+        const t_far = { 
+            x: (rect.right - ray_origin.x) / ray_dir.x,
+            y: (rect.bottom  - ray_origin.y) / ray_dir.y,
+        }
+
+        if(t_near.x > t_far.x) [t_near.x, t_far.x] = [t_far.x, t_near.x];
+        if(t_near.y > t_far.y) [t_near.y, t_far.y] = [t_far.y, t_near.y];
+
         //first filter
-        if(it.near[0] > it.far[1] || it.near[1] > it.far[1]) return false;
-        //update hit near
-        hit_near = Math.max(it.near[0], it.near[1]);
-        //hit far
-        const hit_far  = Math.min(it.far[0], it.far[1]);
+        if(t_near.x > t_far.y || t_near.y > t_far.x) return false;
+        //update hit "timings"
+        t_hit_near = Math.max(t_near.x, t_near.y);
+        if(t_hit_near > 1) return false;
+        const t_hit_far  = Math.min(t_far.x, t_far.y);
         //second filter (collisions behind ray)
-        if(hit_far < 0) return false;
+        if(t_hit_far < 0) return false;
         //calculate collision point 
-        contact_point = getCollisionPoint(ray_origin, hit_near, ray_dir);
+        Object.assign(contact_point, getCollisionPoint(ray_origin, t_hit_near, ray_dir));
         //create normal vector 
-        contact_normal = createNormal(it, ray_dir);
+        if(t_near.x > t_near.y) {
+            if(ray_dir.x < 0) contact_normal.set([1, 0]);
+            else contact_normal.set([-1, 0]);
+        } else if(t_near.x < t_near.y) {
+            if(ray_dir.y < 0) contact_normal.set([0, 1]);
+            else contact_normal.set([0, -1]);
+        }
 
         return true;
 
@@ -74,3 +81,6 @@ const RectangluarCollider = function(  ) {
     }
 
 }
+
+
+export default new RectangluarCollider( );
