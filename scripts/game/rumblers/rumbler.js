d2178ee55a2b5b2b9ed742d5383fd5763f400290
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2023-06-06 08:18:37 -0500

    fixed jump height formula rumbler

--- scripts/game/rumblers/rumbler.js
+++ scripts/game/rumblers/rumbler.js
@@ -5,12 +5,12 @@ GameLib.Rumblers.Rumbler = class extends GameLib.Objects.RigidBody {
         super(width, height, body);
         this.walk_forward_speed = 100;
         this.walk_backward_speed = 80;
-        this.jumpHeight = 30;
+        this.jumpHeight = 70;
     }
 
     jump( gravity ) {
         if(!this.land) return;
-        this.velocity.y = -(2 * gravity * 0.0167) * this.jumpHeight;
+        this.velocity.y = -Math.pow((2 * gravity) * this.jumpHeight, 0.5); //square root -2 times acceleration times distance
     }
 
 }
